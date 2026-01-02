a1cde684022deaa368ab4fd64597f064bedb6710
Rei Khalil S. Galido <rsgalido@student.apc.edu.ph>
2026-01-02 12:07:07 +0800

    test

---
+++ introduction/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    // Toggle Mobile Menu
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+
+    // Close Mobile Menu when a link is clicked
+    navLinks.forEach(n => n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }));
+
+});
